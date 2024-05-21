47fbce4139b2d3bed0a4fadaf48230b52df4d44d
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-05-21 18:30:16 +0700

    fix js and sum layout

--- js/main.js
+++ js/main.js
@@ -2,25 +2,22 @@ const input = document.getElementById('input');
 const realOutput = document.getElementById('output');
 
 input.addEventListener('input', () => {
-  const inputValue = input.value;
+  const inputValue = input.value.trim();
   
   const word = 'miaw';
   let output = '';
   let charIndex = 0;
   
   for (let i = 0; i < inputValue.length; i++) {
-    if (inputValue[i] === ' ') {
+    if (!inputValue[i].match(/[a-zA-Z]/)) {
+      output += inputValue[i];
+    } else if (inputValue[i] === ' ') {
       output += ' ';
     } else {
-      output += word[charIndex % word.length];
-      charIndex++;
+      const currentWordChar = word[charIndex++ % word.length];
+      output += inputValue[i] === inputValue[i].toLowerCase() ? currentWordChar.toLowerCase() : currentWordChar.toUpperCase();
     }
   }
-
-  while (output.length < inputValue.length) {
-    output += word[charIndex % word.length];
-    charIndex++;
-  }
   
-  realOutput.innerText = output || 'Translation ..';
+  realOutput.innerHTML = output || 'Translation ..';
 });
